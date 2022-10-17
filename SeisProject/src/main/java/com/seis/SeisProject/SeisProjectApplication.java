1f69265c04122f13a0e06d7e4493fac03ef1ea33
Luís Eduardo <kauarabelo1@gmail.com>
2022-10-17 09:57:41 -0300

    v1.0

---
+++ SeisProject/src/main/java/com/seis/SeisProject/SeisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.seis.SeisProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeisProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeisProjectApplication.class, args);
+	}
+
+}
---
+++ SeisProject/src/test/java/com/seis/SeisProject/SeisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seis.SeisProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeisProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
